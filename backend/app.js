8efb11006b45b2c331b516c5704f985118c2befc
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-04-13 02:21:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: 'https://next-step-lyart.vercel.app',
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  origin: 'https://next-step-lyart.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],    
+  credentials: true,
 }));
+
 
 // app.options('*', cors());
 app.use(cookieParser());
