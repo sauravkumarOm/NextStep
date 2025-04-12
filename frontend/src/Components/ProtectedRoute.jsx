7aae12db7b000b8e35c7a3b97129739569d26604
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-04-13 02:12:11 +0530

    Update ProtectedRoute.jsx

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:3000"; // Replace with your backend URL
+const BACKEND_URL = "https://nextstep-mb2t.onrender.com"; // Replace with your backend URL
 
 const ProtectedRoute = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
