7d16839122431d03e82f2b52a8ef0dfadb4b3f74
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-04-13 02:05:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://next-step-lyart.vercel.app',
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
