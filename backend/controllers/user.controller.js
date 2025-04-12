4b8e6791f520f59ec159352fcec5c40a3a123396
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-04-13 02:17:15 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -36,19 +36,19 @@ const registerUser = async (req, res) => {
         const token = generateToken(user._id);
 
         // Send token as a cookie
-        // res.cookie("token", token, {
-        //     httpOnly: true,
-        //     secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        //     sameSite: "none",
-        //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        // });
-
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,        // ❌ Don't require HTTPS in dev
-            sameSite: "lax",      // ✅ "lax" works fine locally
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-          });
+            secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: false,        // ❌ Don't require HTTPS in dev
+        //     sameSite: "lax",      // ✅ "lax" works fine locally
+        //     maxAge: 7 * 24 * 60 * 60 * 1000,
+        //   });
           
 
         return res.status(201).json({
@@ -92,19 +92,19 @@ const loginUser = async (req, res) => {
         const token = generateToken(user._id);
 
         // Send token as a cookie
-        // res.cookie("token", token, {
-        //     httpOnly: true,
-        //     secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        //     sameSite: "none",
-        //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        // });
-
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,        // ❌ Don't require HTTPS in dev
-            sameSite: "lax",      // ✅ "lax" works fine locally
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-          });
+            secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: false,        // ❌ Don't require HTTPS in dev
+        //     sameSite: "lax",      // ✅ "lax" works fine locally
+        //     maxAge: 7 * 24 * 60 * 60 * 1000,
+        //   });
           
 
         return res.status(200).json({
