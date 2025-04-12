b3aa36414f8f5224c1d06c2dda173a9aff18283d
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-04-13 02:13:13 +0530

    Update JobPage.jsx

--- frontend/src/Pages/JobPage.jsx
+++ frontend/src/Pages/JobPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { Search, Briefcase, Filter, MapPin, Calendar, ChevronRight } from "lucide-react";
 
-const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = "https://nextstep-mb2t.onrender.com";
 
 const JobSeekerPage = () => {
   const [jobs, setJobs] = useState([]);
